ee2d2b482415e63d8420f34e5cbebe8e4cd142d6
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-01-11 14:18:28 +0700

    update db

--- prod_rekam_medis.sql
+++ prod_rekam_medis.sql
@@ -16,6 +16,87 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`prod_rekam_medis` /*!40100 DEFAULT CHAR
 
 USE `prod_rekam_medis`;
 
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id_user` int NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `admin` */
+
+/*Table structure for table `dokter` */
+
+DROP TABLE IF EXISTS `dokter`;
+
+CREATE TABLE `dokter` (
+  `id_dokter` int NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_dokter`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `dokter` */
+
+/*Table structure for table `pasien` */
+
+DROP TABLE IF EXISTS `pasien`;
+
+CREATE TABLE `pasien` (
+  `id_pasien` int NOT NULL AUTO_INCREMENT,
+  `no_kartu` varchar(255) DEFAULT NULL,
+  `no_pasien` varchar(255) DEFAULT NULL,
+  `jenis_kelamin` varchar(255) DEFAULT NULL,
+  `umur` varchar(255) DEFAULT NULL,
+  `no_telepon` varchar(255) DEFAULT NULL,
+  `alamat` text,
+  `pekerjaan` varchar(255) DEFAULT NULL,
+  `data_state` int DEFAULT NULL,
+  PRIMARY KEY (`id_pasien`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `pasien` */
+
+/*Table structure for table `rekam_medis` */
+
+DROP TABLE IF EXISTS `rekam_medis`;
+
+CREATE TABLE `rekam_medis` (
+  `id_rekam_medis` int NOT NULL AUTO_INCREMENT,
+  `id_pasien` int DEFAULT NULL,
+  `amnesa` text,
+  `diagnosa` text,
+  `tanggal_periksa` date DEFAULT NULL,
+  `tindakan` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_rekam_medis`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `rekam_medis` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `level` enum('1','2','3') DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id_user`,`username`,`password`,`nama`,`level`) values 
+(1,'Sayyid','25d55ad283aa400af464c76d713c07ad','Sayyid','1');
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
