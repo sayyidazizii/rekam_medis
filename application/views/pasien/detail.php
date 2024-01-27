1e91224b65d4fd43b8075885f6df8df29766103c
andikariskys <andikarisky313@gmail.com>
2024-01-27 20:23:32 +0700

    fix detail_pasien

--- application/views/pasien/detail.php
+++ application/views/pasien/detail.php
@@ -1,43 +1,41 @@
 <div class="container-fluid">
     <h1>Data Pasien</h1>
     <table class="table mt-3" style="max-width: 720px; width: 100%;">
-        <?php foreach ($data_pasien as $pasien) : ?>
-            <tr>
-                <th>Nomor Kartu</th>
-                <th>:</th>
-                <td><?= $pasien->no_kartu ?></td>
-            </tr>
-            <tr>
-                <th>Nama Pasien</th>
-                <th>:</th>
-                <td><?= $pasien->nama_pasien ?></td>
-            </tr>
-            <tr>
-                <th>Jenis Kelamin</th>
-                <th>:</th>
-                <td><?= $pasien->jenis_kelamin ?></td>
-            </tr>
-            <tr>
-                <th>Usia</th>
-                <th>:</th>
-                <td><?= $pasien->umur ?></td>
-            </tr>
-            <tr>
-                <th>Nomor HP</th>
-                <th>:</th>
-                <td><?= $pasien->no_telepon ?></td>
-            </tr>
-            <tr>
-                <th>Alamat</th>
-                <th>:</th>
-                <td><?= $pasien->alamat ?></td>
-            </tr>
-            <tr>
-                <th>Pekerjaan</th>
-                <th>:</th>
-                <td><?= $pasien->pekerjaan ?></td>
-            </tr>
-        <?php endforeach; ?>
+        <tr>
+            <th>Nomor Kartu</th>
+            <th>:</th>
+            <td><?= $data_pasien->no_kartu ?></td>
+        </tr>
+        <tr>
+            <th>Nama Pasien</th>
+            <th>:</th>
+            <td><?= $data_pasien->nama_pasien ?></td>
+        </tr>
+        <tr>
+            <th>Jenis Kelamin</th>
+            <th>:</th>
+            <td><?= $data_pasien->jenis_kelamin ?></td>
+        </tr>
+        <tr>
+            <th>Usia</th>
+            <th>:</th>
+            <td><?= $data_pasien->umur ?></td>
+        </tr>
+        <tr>
+            <th>Nomor HP</th>
+            <th>:</th>
+            <td><?= $data_pasien->no_telepon ?></td>
+        </tr>
+        <tr>
+            <th>Alamat</th>
+            <th>:</th>
+            <td><?= $data_pasien->alamat ?></td>
+        </tr>
+        <tr>
+            <th>Pekerjaan</th>
+            <th>:</th>
+            <td><?= $data_pasien->pekerjaan ?></td>
+        </tr>
     </table>
 </div>
 <script>
